03efb730f167714a0b4ed43aedd81f5121d88fe3
gamemasterplc <gamemasterplc@gmail.com>
2018-04-02 20:09:32 -0500

    Accidentally Swapped Super Mario 64 and Mario Kart 64 Folders

--- N64/Super Mario 64/Widescreen/make.bat
+++ N64/Super Mario 64/Widescreen/make.bat
@@ -1,4 +1,4 @@
-ren *.z64 "Mario Kart 64 (U) [!].z64"
-bass.exe "Mario Kart 64 Widescreen.asm"
-rn64crc.exe "Mario Kart 64 Widescreen.z64" -u
-del "Mario Kart 64 (U) [!].z64"
+ren *.z64 "Super Mario 64 (U) [!].z64"
+bass.exe "Super Mario 64 Widescreen.asm"
+rn64crc.exe "Super Mario 64 Widescreen.z64" -u
+del "Super Mario 64 (U) [!].z64"
